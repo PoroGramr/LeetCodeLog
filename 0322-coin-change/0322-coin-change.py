9c6245552089a854d1e4ba4d73cf5fc02c11fd2b
JUNSEO PARK <pjs9177@naver.com>
2025-11-20 17:45:30 +0900

    Time: 683 ms (89.3%), Space: 18.2 MB (68.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -8,7 +8,7 @@ class Solution:
 
         for coin in coins:
             for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x-coin] + 1)
+                dp[x] = min(dp[x], dp[x - coin] + 1)
 
         return dp[amount] if dp[amount] != INF else - 1
         
