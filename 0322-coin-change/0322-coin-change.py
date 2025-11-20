a94181055c43c25cb6ae7a2b512286bd952eeacf
JUNSEO PARK <pjs9177@naver.com>
2025-11-20 17:41:22 +0900

    Time: 721 ms (75.12%), Space: 18.2 MB (68.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,7 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        INF = amount + 1
+        INF = float('inf')
         dp = [INF] * (amount + 1)
 
         dp[0] = 0
