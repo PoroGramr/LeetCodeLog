ca4c9948f09029e79321dc15ae7d1c9007b2cb70
JUNSEO PARK <pjs9177@naver.com>
2025-11-21 13:46:13 +0900

    Time: 129 ms (27.17%), Space: 26.7 MB (99.3%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,12 +2,10 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         answer  = 0
         curMax = 0
-        curMin = float("inf")
+
         prices.reverse()
         for i in range(len(prices)):
             curMax = max(curMax, prices[i])
-            curMin = min(curMin, prices[i])
-
             answer = max(answer, (curMax - prices[i]))
 
         return answer
